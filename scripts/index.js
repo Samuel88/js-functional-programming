7422c9b04b4efc98d1eb8cfe47a018ce31433680
Samuel Panicucci <samuel.panicucci@gmail.com>
2026-04-14 09:05:36 +0200

    Aggiunto un bottone alla home per mostrare il tag audio

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const dom =  {
+    playAudioButton: document.getElementById('playAudio'),
+    audioElement: document.getElementById('myAudio')
+};
+
+function playAudio() {
+    dom.audioElement.play();
+}
+
+dom.playAudioButton.addEventListener('click', playAudio);
